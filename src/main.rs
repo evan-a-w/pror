ad9239c5a7d093b87b65a0cd957ee34a93b88e0d
Evan Williams <evanawilliams@live.com>
2025-08-02 23:47:57 +0800

    😊

--- src/main.rs
+++ src/main.rs
@@ -49,14 +49,14 @@ fn wikipedia() {
 }
 
 pub fn main() {
-    wikipedia();
+    // wikipedia();
     // long()
-    // let formula = dimacs::read_string(dimacs::SUDOKU);
-    // // let formula = vec![vec![1, 2], vec![-2, 3], vec![-1, -3]];
-    // // let formula = dimacs::read_string(dimacs::FAIL_EG);
-    // // let formula = dimacs::read_string(dimacs::SUCC_EG);
-    // // let res = Default::solve(formula);
-    // // let res = DefaultDebug::solve(formula);
-    // let res = State::<FirstSetConfigDebug>::solve(formula);
-    // println! {"res: {:?}", res};
+    let formula = dimacs::read_string(dimacs::SUDOKU);
+    // let formula = vec![vec![1, 2], vec![-2, 3], vec![-1, -3]];
+    // let formula = dimacs::read_string(dimacs::FAIL_EG);
+    // let formula = dimacs::read_string(dimacs::SUCC_EG);
+    // let res = Default::solve(formula);
+    // let res = DefaultDebug::solve(formula);
+    let res = State::<FirstSetConfigDebug>::solve(formula);
+    println! {"res: {:?}", res};
 }
